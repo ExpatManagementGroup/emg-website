6fac37651c98b1efac0b369a615ae0626961348e
saiminh <saiminh@gmail.com>
2024-05-08 20:44:25 +0200

    fix issue with cached video

--- src/components/ui/VideoHero.tsx
+++ src/components/ui/VideoHero.tsx
@@ -1,10 +1,28 @@
 'use client';
 import styles from './VideoHero.module.css';
+import { useEffect } from 'react';
 
 export default function VideoHero( props: { 
   src: string;
   className?: string;
  }) {
+
+  const startVideo = (video: HTMLVideoElement) => {
+    video.play();
+    video.style.opacity = '1';
+  }
+
+  useEffect(() => {
+    const video = document.querySelector('video') as HTMLVideoElement;
+    if (video.readyState > 0) {
+      startVideo(video);
+    } 
+    else {
+      video.addEventListener("loadedmetadata", function(_event) {
+        startVideo(video);
+      });
+  };
+  },[])
 
   const classNames = [styles.videoHero, props.className].join(' ');
 
@@ -19,11 +37,6 @@ export default function VideoHero( props: {
       style={{
         opacity: 0,
         transition: 'opacity 1s ease-in-out'
-      }}
-      onLoadedMetadata={(e) => {
-        const video = e.target as HTMLVideoElement;
-        video.play();
-        video.style.opacity = '1';
       }}
     >
       <source src={props.src} type="video/mp4" />
