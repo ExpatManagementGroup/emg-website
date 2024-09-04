4f52e5513bad47533d151bcc053ca30feaeb20e0
saiminh <saiminh@gmail.com>
2024-09-04 17:15:21 +0200

    conditional quote

--- src/components/our_culture/OurCultureCommunityProject.tsx
+++ src/components/our_culture/OurCultureCommunityProject.tsx
@@ -16,10 +16,12 @@ export default function OurCultureCommunityProject( { props }: { props: any }) {
       <div className={styles.text}>
         <h3 className={styles.name}>{props.name}</h3>
         <div className={styles.description}>{render(props.description)}</div>
-        <blockquote className={styles.quote}>
-          {props.quote}
-          <cite className={styles.cite}>{props.quote_author}</cite>
-        </blockquote>
+        { props.quote && 
+          <blockquote className={styles.quote}>
+            {props.quote}
+            <cite className={styles.cite}>{props.quote_author}</cite>
+          </blockquote>
+        }
       </div>
       
     </div>
