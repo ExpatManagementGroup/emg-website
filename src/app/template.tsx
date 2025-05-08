3cc7d56970bf33e431be312bfcb3f39956b9d3d9
saiminh <saiminh@gmail.com>
2025-05-08 13:46:47 +0200

    no searchParams

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,11 +1,24 @@
 'use client';
 import styles from './template.module.css';
-import ScrollToTop from '@/components/ui/ScrollToTop';
+import { useEffect } from 'react';
+import { usePathname } from 'next/navigation';
 
 export default function Template({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    // Check if the browser window is available (client-side only)
+    if (typeof window !== 'undefined') {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'instant'
+      });
+    }
+  }, [pathname]);
+
   return (
     <div className={styles.transition}>
-      <ScrollToTop />
       {children}
     </div>
   )
